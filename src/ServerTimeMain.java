3f1e1d83caaf7c8c8cf441755147c153147144b7
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2019-07-27 18:51:38 -0700

    update

--- src/ServerTimeMain.java
+++ src/ServerTimeMain.java
@@ -1,5 +1,18 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerTimeMain {
     public static void main(String args[]){
-        System.out.println("Hello Java");    
+        try {
+            ServerSocket ss = new ServerSocket(3600);
+            Socket s = ss.accept();
+            Socket simplesocket = new Socket("192.168.0.8", 3600);
+            System.out.println("it worked");
+        }catch(Exception e){
+            System.out.println("Rip");
+            System.exit(0);
+
+        }
+        System.out.println("Hello Java");
     }
 }
