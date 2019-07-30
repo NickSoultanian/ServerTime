c8bf2e0812fb553ad10d26aa13bf95df43a0c6b2
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2019-07-29 23:45:42 -0700

    alright working code plus rtt

--- src/Client.java
+++ src/Client.java
@@ -10,19 +10,48 @@ import java.net.UnknownHostException;
 *
 */
 public class Client  {
-public static void main(String[] args){
+    private final static String b1 = "C:\\Users\\Nicholas\\Desktop\\1B.txt";
+    private final static String b500 = "C:\\Users\\Nicholas\\Desktop\\500B.txt";
+
+    private final static String b100000 = "C:\\Users\\Nicholas\\Desktop\\10000B.txt";
+    private final static String b800000 = "C:\\Users\\Nicholas\\Desktop\\80000B.txt";
+
+
+    public static void main(String[] args){
     try{
-        Socket s = new Socket("localhost", 3500);//connect to ServerSocket
-        InputStream is = s.getInputStream();  //generate InputStream from Socket
-        byte[] b = new byte[2];
-        is.read(b);  //read byte array into array b
-        System.out.println("byte 1: " + b[0]); //print what was received
-        System.out.println("byte 2: " + b[1]);
+        BufferedReader br = new BufferedReader(new FileReader(b1));
+        String temp;
+        StringBuilder file = new StringBuilder();
+        while(( temp = br.readLine()) != null){
+            file.append(temp);
+        }
+
+        Socket server = new Socket("localhost", 5420);//connect to ServerSocket
+        OutputStream os = server.getOutputStream();
+
+        os.write(file.toString().getBytes());
+        double start = System.nanoTime();
+        os.flush();
+
+        PrintWriter pw = new PrintWriter(new FileWriter("Output1.txt"));
+        InputStream is = server.getInputStream();
+        byte[] b = new byte[file.toString().length()];
+        double end =System.nanoTime();
+        is.read(b);
+
+        double elapse = end-start;
+        System.out.println(elapse);
+
+        for(byte a : b )
+            pw.print(Character.toChars(a));
+
+        os.close();
         is.close();  //close everything
-        s.close();
+        server.close();
 
     }catch(Exception e){
-        System.out.println(e);
+        System.out.println("RIP");
+        System.exit(0);
     }
 }
 }
--- src/ServerTimeMain.java
+++ src/ServerTimeMain.java
@@ -5,52 +5,33 @@ import java.io.*;
 
 public class ServerTimeMain {
     private static ServerSocket server;
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException{
                 try{
+                    server = new ServerSocket(5420);
+                    while(true) {
+                        byte[] data = new byte[2000000]; // since our biggest file is 2x 10^6 byte
+                        Socket s = server.accept();
+                        InputStream in = s.getInputStream();
+                        in.read(data);
 
-                    ServerSocket ss = new ServerSocket(3500);//declare a new ServerSocket on port 3500
-                    Socket s = ss.accept( );  //open the ServerSocket to receive connections
-                    OutputStream os = s.getOutputStream( );  //generate an outputstream from the Socket
-                    byte[] b = new byte[2];
-                    b[0] = 1;
-                    b[1] = 2;
-                    os.write(b);  //write the array
-                    os.close();   //close everything
-                    s.close( );
-                    ss.close( );
 
+                        OutputStream os = s.getOutputStream();
+                        os.write(data);
+                        System.out.println("data " + data);
+                        System.out.println("data length" + data.length);
+                        os.flush();
+                        os.close();
+                        in.close();
+                        s.close();
+                        server.close();
+                    }
                 }catch(Exception e){
-                    System.out.println(e);
+                    System.out.println("RIP");
+                    System.exit(0);
                 }
-            }
+
 
 
         }
 
-
-// Socket simplesocket = new Socket("192.168.0.8", 3600);
-//                System.out.println("it worked");
-//import java.net.*;
-//import java.io.*;
-//class server{
-//    public static void main(String[] args){
-//        try{
-//
-//            ServerSocket ss = new ServerSocket(3500);//declare a new ServerSocket on port 3500
-//            Socket s = ss.accept( );  //open the ServerSocket to receive connections
-//            OutputStream os = s.getOutputStream( );  //generate an outputstream from the Socket
-//            byte[] b = new byte[2];
-//            b[0] = 1;
-//            b[1] = 2;
-//            os.write(b);  //write the array
-//            os.close();   //close everything
-//            s.close( );
-//            ss.close( );
-//
-//        }catch(Exception e){
-//             System.out.println(e);
-//         }
-//   }
-//
-//
-//}
+}
