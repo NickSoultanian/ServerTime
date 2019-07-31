24fa17ee8eec504488a667309bbe9c008d03d1ba
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2019-07-30 19:20:34 -0700

    I dont want to lose my work

--- src/Client.java
+++ src/Client.java
@@ -12,10 +12,19 @@ import java.net.UnknownHostException;
 public class Client  {
     private final static String b1 = "C:\\Users\\Nicholas\\Desktop\\1B.txt";
     private final static String b500 = "C:\\Users\\Nicholas\\Desktop\\500B.txt";
+    private final static String b10000 = "C:\\Users\\Nicholas\\Desktop\\10000B.txt";
+    private final static String b80000 = "C:\\Users\\Nicholas\\Desktop\\80000B.txt";
+    private final static String b400000 = "C:\\Users\\Nicholas\\Desktop\\400000B.txt";
+    private final static String b2000000= "C:\\Users\\Nicholas\\Desktop\\2000000B.txt";
 
-    private final static String b100000 = "C:\\Users\\Nicholas\\Desktop\\10000B.txt";
-    private final static String b800000 = "C:\\Users\\Nicholas\\Desktop\\80000B.txt";
+    private final static double rTT1B = 1;
 
+    private final static double b1Size = 1;
+    private final static double b500Size = 500;
+    private final static double b10000Size = 10000;
+    private final static double b80000Size = 80000;
+    private final static double b400000Size = 400000;
+    private final static double b2000000Size = 2000000;
 
     public static void main(String[] args){
     try{
@@ -39,8 +48,15 @@ public class Client  {
         double end =System.nanoTime();
         is.read(b);
 
-        double elapse = end-start;
-        System.out.println(elapse);
+        double rTT = end-start;
+        System.out.println("Here is Round Trip Time: " + rTT);
+
+        double throughPut = b1Size / rTT;
+        System.out.println("Here is ThroughPut: " + throughPut);
+
+        double effectiveBandwidth = (b1Size/(rTT-rTT1B));
+        System.out.println("Here is the Effective Bandwidth for a" + b1Size + "B file: ");
+
 
         for(byte a : b )
             pw.print(Character.toChars(a));
--- src/ServerTimeMain.java
+++ src/ServerTimeMain.java
@@ -8,28 +8,27 @@ public class ServerTimeMain {
     public static void main(String args[]) throws IOException{
                 try{
                     server = new ServerSocket(5420);
-                    while(true) {
-                        byte[] data = new byte[2000000]; // since our biggest file is 2x 10^6 byte
-                        Socket s = server.accept();
-                        InputStream in = s.getInputStream();
-                        in.read(data);
+
+                    byte[] data = new byte[2000000]; // since our biggest file is 2x 10^6 byte
+                    Socket s = server.accept();
+                    InputStream in = s.getInputStream();
+                    in.read(data);
 
 
-                        OutputStream os = s.getOutputStream();
-                        os.write(data);
-                        System.out.println("data " + data);
-                        System.out.println("data length" + data.length);
-                        os.flush();
-                        os.close();
-                        in.close();
-                        s.close();
-                        server.close();
-                    }
+                    OutputStream os = s.getOutputStream();
+                    os.write(data);
+                    System.out.println("data " + data);
+                    System.out.println("data length" + data.length);
+                    os.flush();
+                    os.close();
+                    in.close();
+                    s.close();
+                    server.close();
+                    
                 }catch(Exception e){
                     System.out.println("RIP");
                     System.exit(0);
                 }
-
 
 
         }
